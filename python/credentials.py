c9da3996092ebdaa9890dbe2b7d714cbc901cb71
Stanislav Vassiljev <stanvas@gmail.com>
2020-12-09 12:13:44 +0200

    second entry

---
+++ python/credentials.py
@@ -0,0 +1,6 @@
+
+account_sid = "ACCOUNT_SID"
+auth_token = "AUTH_TOKEN"
+
+sender = "whatsapp:+14155238886"
+receiver = "RECEIVER_NUMBER"
--- python/messaging.py
+++ python/messaging.py
@@ -6,7 +6,7 @@ from twilio.rest import Client
 client = Client(cred.account_sid, cred.auth_token)
 
 message = client.messages.create(
-                              from_ = "whatsapp:+14155238886",
+                              from_ = cred.sender,
                               body = 'Hello world!',
                               to = cred.receiver
                           )
